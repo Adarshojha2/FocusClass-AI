51634ee449b8480dc172c83ef35d89fa0277a96f
Adarsh Kumar Ojha <adarshojha999@gmail.com>
2026-06-13 17:56:20 +0530

    fix: rename authmiddleware to authMiddleware to fix Linux case sensitivity crash

--- backend/routes/feedbackRoutes.js
+++ backend/routes/feedbackRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { sendFeedback } = require('../controllers/feedbackController');
-const authMiddleware = require('../middleware/authmiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/', authMiddleware, sendFeedback);
 
